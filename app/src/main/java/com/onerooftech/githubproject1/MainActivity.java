ea4fe23540ca87df062e08d5fd21c5f8cfad9b51
DESKTOP-QDOP64T\suraj <telagadesuraj581998@gmail,com>
2022-12-02 17:23:52 +0530

    Commit new Branch2 one

--- app/src/main/java/com/onerooftech/githubproject1/MainActivity.java
+++ app/src/main/java/com/onerooftech/githubproject1/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Welcome Suraj");
+        System.out.println("Welcome newBranch 2");
     }
 }
