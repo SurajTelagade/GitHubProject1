14497b87de1de8bd3aee41b961e57036b874c614
DESKTOP-QDOP64T\suraj <telagadesuraj581998@gmail,com>
2022-12-02 18:23:07 +0530

    Commit activity 4

---
+++ app/src/main/java/com/onerooftech/githubproject1/MainActivity4.java
@@ -0,0 +1,15 @@
+package com.onerooftech.githubproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        System.out.println("Main Activity 4");
+    }
+}
