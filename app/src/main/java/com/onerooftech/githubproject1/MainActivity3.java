22bc8490a1c29755890b79b5de5ac4b854661a41
DESKTOP-QDOP64T\suraj <telagadesuraj581998@gmail,com>
2022-12-02 18:17:44 +0530

    Commit  Activity 3

---
+++ app/src/main/java/com/onerooftech/githubproject1/MainActivity3.java
@@ -0,0 +1,15 @@
+package com.onerooftech.githubproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        System.out.println("Main Activity 3");
+    }
+}
