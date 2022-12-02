7acb5ebf0bc0aea065742002348856057fce1aa1
DESKTOP-QDOP64T\suraj <telagadesuraj581998@gmail,com>
2022-12-02 18:30:55 +0530

    Commit activity 5

---
+++ app/src/main/java/com/onerooftech/githubproject1/MainActivity5.java
@@ -0,0 +1,15 @@
+package com.onerooftech.githubproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+        System.out.println("Main Activity 5");
+    }
+}
